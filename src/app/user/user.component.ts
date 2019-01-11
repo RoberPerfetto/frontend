db249765cbed54ae11aba1c327720668cc727e17
RoberPerfetto <rober_perfe@hotmail.com>
2019-01-11 04:17:18 -0300

    My project is working, I have created my routing module and my user component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { UserServiceService } from './user-service.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+  providers: [UserServiceService]
+})
+export class UserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
